e6c563db739c30aa8fb506564f2350728d7e5230
OVElanggui <62495713+OVElanggui@users.noreply.github.com>
2020-07-14 18:55:04 +0800

    Add files via upload

---
+++ 远程下载.ps1
@@ -0,0 +1,11 @@
+﻿<#if((Get-WmiObject win32_operatingsystem).osarchitecture.contains('32')){
+    Write-Host "32"
+    #iex(New-Object net.webclient).DownloadString("http://jvniu.jb51.net:81/201604/books/IntermediatePython(jb51.net).rar")
+}else{#>
+    #Write-Host "64"
+    #iex(New-Object net.webclient).DownloadString("http://jvniu.jb51.net:81/201604/books/IntermediatePython(jb51.net).rar","D:\IntermediatePython(jb51.net).rar")
+   # $client = new-object Net.WebClient
+   # $client.DownloadFile('http://jvniu.jb51.net:81/201604/books/IntermediatePython(jb51.net).rar', 'D:\IntermediatePython(jb51.net).rar')
+    $client = new-object Net.WebClient
+    $client.DownloadFile('https://codeload.github.com/OVElanggui/aiya/zip/aa', 'D:\aiya-aa.zip')
+#}
